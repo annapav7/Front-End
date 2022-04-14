1964ffea783e8ae93dd977b3da372723177d1f94
Hayes McCardell II <hmccardell@catalyte.io>
2022-04-14 11:36:42 -0500

    initial project commit

---
+++ src/Catalyte.Apparel.API/Controllers/PurchaseController.cs
@@ -0,0 +1,62 @@
+﻿﻿using AutoMapper;
+using Catalyte.Apparel.API.DTOMappings;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalyte.Apparel.DTOs.Purchases;
+using Catalyte.Apparel.Providers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Catalyte.Apparel.API.Controllers
+{
+    /// <summary>
+    /// The PurchasessController exposes endpoints for purchase related actions.
+    /// </summary>
+    [ApiController]
+    [Route("/purchases")]
+    public class PurchaseController : ControllerBase
+    {
+        private readonly ILogger<PurchaseController> _logger;
+        private readonly IPurchaseProvider _purchaseProvider;
+        private readonly IMapper _mapper;
+
+        public PurchaseController(
+            ILogger<PurchaseController> logger,
+            IPurchaseProvider purchaseProvider,
+            IMapper mapper
+        )
+        {
+            _logger = logger;
+            _purchaseProvider = purchaseProvider;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PurchaseResponseDTO>>> GetAllPurchasesAsync()
+        {
+            _logger.LogInformation("Request received for GetAllPurchasesAsync");
+
+            var purchases = await _purchaseProvider.GetAllPurchasesAsync();
+            var purchaseResponseDTOs = _mapper.MapPurchasesToPurchaseResponseDTOs(purchases);
+
+            return Ok(purchaseResponseDTOs);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<PurchaseResponseDTO>>> CreatePurchaseAsync([FromBody] PurchaseRequestDTO model)
+        {
+            _logger.LogInformation("Request received for CreatePurchase");
+
+            var newPurchase = _mapper.MapCreatePurchaseDTOToPurchase(model);
+            var savedPurchase = await _purchaseProvider.CreatePurchaseAsync(newPurchase);
+            var purchaseResponseDTO = _mapper.MapPurchaseToPurchaseResponseDTO(savedPurchase);
+
+            if (purchaseResponseDTO != null)
+            {
+                return NoContent();
+            }
+
+            return Created($"/purchases/", purchaseResponseDTO);
+        }
+    }
+}
---
+++ src/Catalyte.Apparel.Data/Models/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Catalyte.Apparel.Data.Models
+{
+    /// <summary>
+    /// This class represents a base for all other entities.
+    /// </summary>
+    public class BaseEntity
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+    }
+
+}
